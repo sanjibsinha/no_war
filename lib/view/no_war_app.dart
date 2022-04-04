8389b1af02015149a076ec7443a5dd64d23bb9ac
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-04-04 06:59:58 +0530

    Second

--- lib/view/no_war_app.dart
+++ lib/view/no_war_app.dart
@@ -9,11 +9,13 @@ class NoWarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      home: const NoWarHomePage(),
+      title: 'Modern War History',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF3EBACE),
+        backgroundColor: const Color(0xFFF3F5F7),
       ),
-      home: const NoWarHomePage(title: 'No War'),
     );
   }
 }
